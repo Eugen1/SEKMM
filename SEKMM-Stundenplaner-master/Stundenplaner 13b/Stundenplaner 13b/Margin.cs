15753425aa6f9c798a0b00c350bd36530981baa5
Eugen1 <eugen-1994@hotmail.de>
2013-11-01 23:11:07 +0100

    01.11.13

---
+++ SEKMM-Stundenplaner-master/Stundenplaner 13b/Stundenplaner 13b/Margin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stundenplaner_13b
+{
+    class Margin
+    {
+        private int p1;
+        private int p2;
+        private int p3;
+        private int p4;
+
+        public Margin(int p1, int p2, int p3, int p4)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.p4 = p4;
+        }
+
+        public Margin()
+        {
+            // TODO: Complete member initialization
+        }
+    }
+}
